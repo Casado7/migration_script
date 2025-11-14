95b15d8ec6905143662951e9083875ecbe02c7d9
Alejandro Casado <alejandroacasadoc@gmail.com>
2025-11-14 15:27:59 -0400

    refactor: rename create_client import for consistency in target helpers

--- src/target_helppers/__init__.py
+++ src/target_helppers/__init__.py
@@ -1,7 +1,7 @@
 """Helpers for interacting with the target application."""
 
 from .login import fill_and_submit_login, start_and_login
-from .insert_client import navigate_to_add_client_page, create_test_client
+from .insert_client import navigate_to_add_client_page, create_client as create_test_client
 
 __all__ = [
 	"fill_and_submit_login",
